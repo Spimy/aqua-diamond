c6058f479ead729f2469ea64a7008bc5e5763295
Spimy <officialspyme@gmail.com>
2020-07-11 23:35:05 +0400

    Handle server info

--- ts/script.ts
+++ ts/script.ts
@@ -1,9 +1,9 @@
-const url = `https://mcapi.us/server/status?ip=play.hypixel.net&port=25565`
+import { config } from './config.js';
 
 const toggleMenu = (opened: boolean = false) => {
 
     const nav = document.getElementById("nav")!;
-    const button = document.getElementById("close-btn")!;
+    const button = <HTMLButtonElement>document.getElementById("close-btn")!;
 
     if (!nav.classList.contains("open") && !opened) {
         nav.classList.add("open");
@@ -17,6 +17,65 @@ const toggleMenu = (opened: boolean = false) => {
 
 }
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
     if (window.innerWidth > 720) toggleMenu(true);
+});
+
+interface ServerInfo {
+    online: boolean;
+    players: { max: number, now: number };
+
+}
+
+const showMsgBox = (msgBox: HTMLDivElement) => {
+
+    msgBox?.classList.add("show");
+    setTimeout(() => {
+        msgBox?.classList.remove("show");
+    }, 5000);
+
+}
+
+const copyIP = () => {
+
+    const msgBox = <HTMLDivElement>document.getElementById("msg_box");
+
+    navigator.clipboard.writeText(config.serverIP).then(() => {
+        msgBox.innerHTML = "Copied IP to Clipboard";
+        showMsgBox(msgBox);
+    }).catch(() => {
+        msgBox.innerHTML = "Error Copying IP";
+        showMsgBox(msgBox);
+    });
+
+}
+
+window.addEventListener("load", async () => {
+    const title = <HTMLHeadingElement>document.getElementById("title")!;
+    const subtitle = <HTMLParagraphElement>document.getElementById("subtitle")!;
+
+    title.innerHTML = `Welcome to ${config.serverName}`;
+    subtitle.getElementsByClassName("server-ip")[0].innerHTML = config.serverIP;
+
+    setInterval(async () => {
+        const url = `https://mcapi.us/server/status?ip=${config.serverIP}&port=${config.serverPort}`
+        const server: ServerInfo = await fetch(url, { method: "GET" })
+            .then(res => res.json())
+            .then(data => data);
+
+        if (!server.online) {
+            subtitle.innerHTML = "Server offline";
+        } else {
+            subtitle.getElementsByClassName("player-count")[0].innerHTML = server.players.now.toString();
+        }
+
+    }, 3000);
+
+
+    const serverIP = <HTMLSpanElement>subtitle.getElementsByClassName("server-ip")[0];
+    serverIP?.addEventListener("click", copyIP);
+
+    const btn = <HTMLButtonElement>document.getElementById("close-btn");
+    btn?.addEventListener("click", () => toggleMenu())
+
 });
